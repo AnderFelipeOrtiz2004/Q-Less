bebeae419437ea1e72e9117bdfe9ee5343407b34
Anderson-Felipe-2004 <ortizgarciafelipe37@gmail.com>
2026-06-09 00:18:49 -0500

    Update entrypoint script to use apache2-foreground

--- mobile-api/docker-entrypoint.sh
+++ mobile-api/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -e
-
 PORT="${PORT:-8080}"
-sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-available/000-default.conf
 
-exec "$@"
+sed -i "s/^Listen 80$/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -i "s/^<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+
+exec apache2-foreground
